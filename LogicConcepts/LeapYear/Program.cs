50082ce96e1224dfb6b4140647605377e8bf33a3
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-21 20:37:40 -0500

    Is leap year

---
+++ LogicConcepts/LeapYear/Program.cs
@@ -0,0 +1,30 @@
+﻿using Shared;
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese un año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year < currentYear)
+    {
+        message = "fue";
+    }
+    else
+    {
+        message = "va a ser";
+    }
+
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    {
+        Console.WriteLine($"El año {year} {message} un año bisiesto.");
+    }
+    else
+    {
+        Console.WriteLine($"El año {year} no {message} un año bisiesto.");
+    }
+} while (true);
