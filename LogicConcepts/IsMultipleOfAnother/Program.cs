fbacdcec2f84bd905d6a581c29057269e61127ec
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-21 20:21:55 -0500

    Is Multiple of another number

---
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    if (a % b == 0)
+    {
+        Console.WriteLine($"{a} es múltiplo de {b}");
+    }
+    else if (b % a == 0)
+    {
+        Console.WriteLine($"{b} es múltiplo de {a}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} y {b} no son múltiplos entre sí.");
+    }
+}
+while (true);
