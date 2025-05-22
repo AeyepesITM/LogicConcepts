5ffa9bf917b21a508c042a3f64d847fc4bbe9eff
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-22 18:33:28 -0500

    Factorial

---
+++ LogicConcepts/Shared/MyMath.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared;
+
+public class MyMath
+{
+    public static double Factorial(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException("El número debe ser mayor o igual a cero.");
+        }
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+}
