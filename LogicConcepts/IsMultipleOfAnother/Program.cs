f5936852aa1c974c68e7634bd9541903b29b2aa0
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-21 21:01:11 -0500

    Include exits programms

--- LogicConcepts/IsMultipleOfAnother/Program.cs
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Shared;
 
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
 do
 {
     var a = ConsoleExtension.GetInt("Ingrese primer número: ");
@@ -16,5 +19,10 @@ do
     {
         Console.WriteLine($"{a} y {b} no son múltiplos entre sí.");
     }
-}
-while (true);
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
--- LogicConcepts/SortNumbers/Program.cs
+++ LogicConcepts/SortNumbers/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Shared;
 
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
 do
 {
     Console.WriteLine("Ingrese 3 números diferentes...");
@@ -41,4 +44,10 @@ do
     {
         Console.WriteLine($"El número mayor es {c}, el medio es {b}, el menor es {a}");
     }
-} while (true);
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
