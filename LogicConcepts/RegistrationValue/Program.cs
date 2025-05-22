b18e5590e233d4cd1cb9256e1d5d8ab3beca6477
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-21 23:10:23 -0500

    Calculate registration value

---
+++ LogicConcepts/RegistrationValue/Program.cs
@@ -0,0 +1,74 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+do
+{
+    var quantity = ConsoleExtension.GetFloat("Ingrese el número de créditos matriculados: ");
+    var valueToCredit = ConsoleExtension.GetDecimal("Ingrese el valor por cada crédito: ");
+    var stratum = ConsoleExtension.GetInt("Ingrese el estrato socioeconómico: ");
+    var valueToPay = CalculateRegistrationValue(quantity, valueToCredit, stratum);
+    decimal subsidy = CalculateSubsidy(stratum);
+
+    Console.WriteLine($"El valor a pagar es: {valueToPay:C2}");
+    Console.WriteLine($"El subsidio es: {subsidy:C2}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+decimal CalculateRegistrationValue(float credits, decimal valueUnit, int stratum)
+{
+    decimal valueUnit1 = valueUnit;
+    decimal valueRegistration = 0;
+    var discount = 0d;
+    while (credits <= 0)
+    {
+        Console.WriteLine("El número de créditos matriculados no puede ser negativo.");
+        credits = ConsoleExtension.GetFloat("Ingrese el número de créditos matriculados: ");
+    }
+
+    if (credits > 20)
+    {
+        //valueRegistration =  20 * valueUnit1 + ((decimal)credits - 20) * valueUnit1 * 2;
+        valueRegistration = 2 * valueUnit1 * ((decimal)credits - 10);
+    }
+    else
+    {
+        valueRegistration = (decimal)credits * valueUnit1;
+    }
+    if (stratum > 0)
+    {
+        if (stratum < 2)
+        {
+            discount = 0.8d;
+        }
+        else if (stratum < 3)
+        {
+            discount = 0.5d;
+        }
+        else if (stratum < 4)
+        {
+            discount = 0.3d;
+        }
+    }
+    valueRegistration = valueRegistration * (1 - (decimal)discount);
+
+    return valueRegistration;
+}
+
+decimal CalculateSubsidy(int stratum)
+{
+    decimal subsidyCalculate = 0;
+    if (stratum < 2)
+    {
+        subsidyCalculate = 200000;
+    }
+    else if (stratum < 3)
+    {
+        subsidyCalculate = 100000;
+    }
+    return subsidyCalculate;
+}
