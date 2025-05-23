e8ba7daabe80d2824402f33a0c48fef1bd323c51
ANDRES EDUARDO YEPES CORREA <andresyepes138092@correo.itm.edu.co>
2025-05-22 20:23:55 -0500

    Fibonacci

--- LogicConcepts/Shared/MyMath.cs
+++ LogicConcepts/Shared/MyMath.cs
@@ -14,4 +14,21 @@ public class MyMath
         }
         return n * Factorial(n - 1);
     }
+
+    public static double Fibonacci(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException("El número debe ser mayor o igual a cero.");
+        }
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
 }
